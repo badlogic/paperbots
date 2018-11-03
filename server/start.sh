303b2baca5d9ebfc77d1495fa119dd1e0ff5ed37
badlogic <contact@badlogicgames.com>
2018-11-03 13:58:26 +0100

    Fixed up start script for Docker container.

--- server/start.sh
+++ server/start.sh
@@ -3,5 +3,5 @@ while :
 do
 git pull
 mvn clean package -Dmaven.test.skip=true -U
-java -jar target/paperbots.jar -s ../client/site
+java -jar target/paperbots.jar -s ../client/site -c $PAPERBOTS_CONFIG
 done
